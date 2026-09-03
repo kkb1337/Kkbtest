f3e848351b7932df2b4c3e8f9ca1efd17ed4cb54
kkb1337 <kkb1337228@gmail.com>
2026-09-03 22:22:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ftracker-v1.4.8-index-ui-hotfix';
+const CACHE_VERSION = 'ftracker-v1.4.9-index-ux-consolidation';
 const CACHE_NAME = CACHE_VERSION;
 const APP_SHELL = [
   './',
