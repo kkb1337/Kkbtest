b0f62aac60d1d3df8ae90c7f82e734d4b445d619
kkb1337 <kkb1337228@gmail.com>
2026-09-04 21:47:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='ftracker-v1.4.30-unified-window-design';
+const CACHE_VERSION='ftracker-v1.4.31-unified-window-design';
 const CACHE_NAME=CACHE_VERSION;
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil((async()=>{const c=await caches.open(CACHE_NAME);await c.addAll(APP_SHELL);await self.skipWaiting();})()));
